1bd16eb1f9935c8848628551520f34afcd93ed10
danstarns <danielstarns@hotmail.com>
2024-04-24 17:48:19 +0700

    schema: add title

--- prisma/migrations/20240424103533_dev/migration.sql
+++ prisma/migrations/20240424103533_dev/migration.sql
@@ -8,6 +8,7 @@ CREATE TABLE "User" (
 -- CreateTable
 CREATE TABLE "Post" (
     "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "authorId" TEXT NOT NULL,
     CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
